3ee0ad1dde9559e2956880f96ecf7231e0178a41
Samyukthaa <asksamyukthaa@gmail.com>
2026-01-09 13:25:10 +0530

    Initial Spring Boot + MySQL setup (Member 1)

---
+++ src/main/java/com/wisewallet/backend/WiseWalletBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wisewallet.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseWalletBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseWalletBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wisewallet/backend/WiseWalletBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisewallet.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseWalletBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
